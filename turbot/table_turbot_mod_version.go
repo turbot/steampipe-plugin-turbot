238ae8e9dbf0a1c9d9b96f9936c8e98e9c7ee8a4
ParthaI <partha@turbot.com>
2022-02-01 12:14:20 +0530

    Updated the table description

--- turbot/table_turbot_mod_version.go
+++ turbot/table_turbot_mod_version.go
@@ -29,7 +29,7 @@ func tableTurbotModVersion(ctx context.Context) *plugin.Table {
 			{Name: "org_name", Type: proto.ColumnType_STRING, Transform: transform.FromQual("org_name"), Description: "The name of the organization."},
 			{Name: "status", Type: proto.ColumnType_STRING, Description: "The status of the mod version."},
 			{Name: "version", Type: proto.ColumnType_STRING, Description: "The version of the mod."},
-			{Name: "filter", Type: proto.ColumnType_STRING, Transform: transform.FromQual("filter"), Description: "Search text used for this mod list."},
+			{Name: "filter", Type: proto.ColumnType_STRING, Transform: transform.FromQual("filter"), Description: "Filter used to search for mod versions."},
 			{Name: "mod_peer_dependency", Type: proto.ColumnType_JSON, Transform: transform.FromField("Head.PeerDependencies"), Description: "Peer dependencies of the mod."},
 			// Other columns
 			{Name: "workspace", Type: proto.ColumnType_STRING, Hydrate: plugin.HydrateFunc(getTurbotWorkspace).WithCache(), Transform: transform.FromValue(), Description: "Specifies the workspace URL."},
@@ -92,13 +92,9 @@ func listModVersion(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateD
 		orgName = quals["org_name"].GetStringValue()
 	}
 
-	// Default to a very large page size. Page sizes earlier in the filter string
-	// win, so this is only used as a fallback.
-	pageResults := false
-
 	plugin.Logger(ctx).Trace("turbot_mod_version.listModVersion", "quals", quals)
-
 	nextToken := ""
+
 	for {
 		result := &ModVersionResponse{}
 		if status != nil {
@@ -128,10 +124,9 @@ func listModVersion(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateD
 				return nil, nil
 			}
 		}
-		if !pageResults || result.ModVersionSearches.Paging.Next == "" {
+		if result.ModVersionSearches.Paging.Next == "" {
 			break
 		}
-		nextToken = result.ModVersionSearches.Paging.Next
 	}
 
 	return nil, nil
